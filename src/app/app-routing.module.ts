eedad5223a602cbba2a96ddbdbafabced99b57e7
leonardo <84354110+lrivas3@users.noreply.github.com>
2023-02-14 21:27:39 -0600

    Project finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MedicComponent } from './pages/medic/medic.component';
 import { PatientComponent } from './pages/patient/patient.component';
+import { SearchComponent } from './pages/search/search.component';
+
 
 
 const routes: Routes = [
   {path: 'pages/medic', component: MedicComponent},
-  {path: 'pages/patient', component: PatientComponent}
+  {path: 'pages/patient', component: PatientComponent},
+  {path: 'pages/search', component: SearchComponent}
   ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+/** From Material */
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+
+/**Components */
 import { PatientComponent } from './pages/patient/patient.component';
 import { MedicComponent } from './pages/medic/medic.component';
 
@@ -32,6 +35,7 @@ import { MedicComponent } from './pages/medic/medic.component';
     MatSidenavModule,
     MatMenuModule,
     MatDividerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
