67d30aff59f328b4b61ca5d901c78c1b77134ebb
leonardo <84354110+lrivas3@users.noreply.github.com>
2023-02-14 19:57:33 -0600

    Before sending

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MedicComponent } from './pages/medic/medic.component';
+import { PatientComponent } from './pages/patient/patient.component';
 
+
+const routes: Routes = [
+  {path: 'pages/medic', component: MedicComponent},
+  {path: 'pages/patient', component: PatientComponent}
+  ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReportComponent } from './pages/report/report.component';
-import { SearchComponent } from './pages/search/search.component';
-import { ConsultComponent } from './pages/consult/consult.component';
-import { ConsultEspecialComponent } from './pages/consult-especial/consult-especial.component';
-import { ConsultWizardComponent } from './pages/consult-wizard/consult-wizard.component';
-import { SpecialityComponent } from './pages/speciality/speciality.component';
-import { MedicComponent } from './pages/medic/medic.component';
-import { ExamComponent } from './pages/exam/exam.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 import { PatientComponent } from './pages/patient/patient.component';
+import { MedicComponent } from './pages/medic/medic.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReportComponent,
-    SearchComponent,
-    ConsultComponent,
-    ConsultEspecialComponent,
-    ConsultWizardComponent,
-    SpecialityComponent,
-    MedicComponent,
-    ExamComponent,
-    PatientComponent
+    PatientComponent,
+    MedicComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
